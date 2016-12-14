b468e17e933e39d52dc2761376222d6a661062dd
Mehnert Stefan <s.mehnert@polipol.de>
2016-12-14 05:28:46 +0000

    Added routing informations the the Component metadata

--- webapp/Component.js
+++ webapp/Component.js
@@ -9,6 +9,34 @@ sap.ui.define([
 			"rootView": "sapui5.demo.mvcapp.view.App",
 			"config": {
 				"serviceUrl": "webapp/service/data.json"
+			},
+			"routing": {
+				"routerClass": "sap.m.routing.Router",
+				"viewType": "XML",
+				"viewPath": "sapui5.demo.mvcapp.view",
+				"controlId": "app",
+				"controlAggregation": "pages",
+				"transition": "slide"
+			},
+			"routes": [{
+				"pattern": "",
+				"name": "master",
+				"target": "mater" 
+			},
+			{
+				"pattern": "detail/{ID}",
+				"name": "detail",
+				"target": "detail"
+			}],
+			"targets": {
+				"master": {
+					"viewName": "master",
+					"viewLevel": 1
+				},
+				"detail": {
+					"viewName": "Detail",
+					"viewLevel": 2
+				}
 			}
 		},
 		createContent: function() {
