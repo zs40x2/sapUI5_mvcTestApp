9bc147be68332b7b82cd10b65fd9f852aeefe592
Mehnert Stefan <s.mehnert@polipol.de>
2016-12-14 05:55:45 +0000

    Fixed Component routing configuration

--- webapp/Component.js
+++ webapp/Component.js
@@ -4,53 +4,57 @@ sap.ui.define([
 	], function(UIComponent, JSONModel) {
 		"use strict";
 	
-	return UIComponent.extend("sapui5.demo.mvcapp.App", {
+	return UIComponent.extend("sapui5.demo.mvcapp.Component", {
+
 		metadata: {
 			"rootView": "sapui5.demo.mvcapp.view.App",
 			"config": {
 				"serviceUrl": "webapp/service/data.json"
 			},
 			"routing": {
-				"routerClass": "sap.m.routing.Router",
-				"viewType": "XML",
-				"viewPath": "sapui5.demo.mvcapp.view",
-				"controlId": "app",
-				"controlAggregation": "pages",
-				"transition": "slide"
-			},
-			"routes": [{
-				"pattern": "",
-				"name": "master",
-				"target": "mater" 
-			},
-			{
-				"pattern": "detail/{ID}",
-				"name": "detail",
-				"target": "detail"
-			}],
-			"targets": {
-				"master": {
-					"viewName": "master",
-					"viewLevel": 1
+				"config": {
+					"routerClass": "sap.m.routing.Router",
+					"viewType": "XML",
+					"viewPath": "sapui5.demo.mvcapp.view",
+					"controlId": "app",
+					"controlAggregation": "pages",
+					"transition": "slide"
 				},
-				"detail": {
-					"viewName": "Detail",
-					"viewLevel": 2
+				"routes": [{
+					"pattern": "",
+					"name": "master",
+					"target": "master" 
+				},
+				{
+					"pattern": "detail/{ID}",
+					"name": "detail",
+					"target": "detail"
+				}],
+				"targets": {
+					"master": {
+						"viewName": "Master",
+						"viewLevel": 1
+					},
+					"detail": {
+						"viewName": "Detail",
+						"viewLevel": 2
+					}
 				}
 			}
 		},
 		init: function() {
 			UIComponent.prototype.init.apply(this, arguments);
 			
+			var router = this.getRouter();
 			this.getRouter().initialize();
 		},
 		createContent: function() {
-			var oRootView = UIComponent.prototype.createContent.apply(this, arguments);
+			/*var oRootView = UIComponent.prototype.createContent.apply(this, arguments);
 			
             this.setModel(new JSONModel(this.getMetadata().getConfig().serviceUrl));
            
             oApp = oRootView.byId("app");
-            return oRootView;
+            return oRootView;*/
 		}
 	});	
 });
