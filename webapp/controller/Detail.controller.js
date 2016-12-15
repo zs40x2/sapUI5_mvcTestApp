721ccbb7cdb0a940484fd8a60bc0f641a55d7633
Mehnert Stefan <s.mehnert@polipol.de>
2016-12-15 05:26:13 +0000

    Added the the browser history

--- webapp/controller/Detail.controller.js
+++ webapp/controller/Detail.controller.js
@@ -1,6 +1,7 @@
 sap.ui.define([
-	"sap/ui/core/mvc/Controller"
-], function(Controller) {
+	"sap/ui/core/mvc/Controller",
+	"sap/ui/core/routing/History"
+], function(Controller, History) {
 	"use strict";
 	
 	return Controller.extend("sapui5.demo.mvcapp.controller.Detail", {
