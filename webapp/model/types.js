484492223b5fa87928f99df8bba32feab010b677
Mehnert Stefan <s.mehnert@polipol.de>
2017-01-05 05:21:26 +0000

    PhoneNumber type declaration

--- webapp/model/types.js
+++ webapp/model/types.js
@@ -4,5 +4,18 @@ sap.ui.define([
 		"use strict";
 		return {
 			
+			PhoneNumber : SimpleType.extend("sap.test.phoneNumber", {
+				formatValue: function(oValue) {
+					return "Phone number:" + oValue;
+				},
+				parseValue: function(oValue) {
+					return oValue;
+				},
+				validateValue: function(oValue) {
+					if (!/\+*\D*[0-9]*\-([2-9]\d{2})(\D*)([2-9]\d{2})(\D*)(\d{4})\D/.test(oValue)) {
+        				throw new sap.ui.model.ValidateException("phone number must follow the pattern +1 234-567-890!");
+        			}
+				}
+			})
 		};
 	});
