3b5f7651c18c98eeae6113230a2ee20ce9784198
Mehnert Stefan <s.mehnert@polipol.de>
2017-01-09 05:39:33 +0000

    Added an empy notFoundController

---
+++ webapp/controller/NotFound.controller.js
@@ -0,0 +1,9 @@
+sap.ui.define([
+	"sapui5/demo/mvcapp/controller/BaseController"
+], function(BaseController) {
+	"use strict";
+	
+	return BaseController.extend("sapui5.demo.mvcapp.controller.NotFound", {
+		
+	});
+});
