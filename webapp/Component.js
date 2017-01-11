4e03b05f2520c6dd65d3f06854db645858fbef86
Mehnert Stefan <s.mehnert@polipol.de>
2017-01-11 05:37:37 +0000

    Load deviceModel in component initialization.

--- webapp/Component.js
+++ webapp/Component.js
@@ -1,6 +1,8 @@
 sap.ui.define([
-	"sap/ui/core/UIComponent"
-	], function(UIComponent) {
+	"sap/ui/core/UIComponent",
+	"sap/ui/model/json/JSONModel",
+	"sap/ui/Device"
+	], function(UIComponent, JSONModel, Device) {
 		"use strict";
 	
 	return UIComponent.extend("sapui5.demo.mvcapp.Component", {
@@ -10,6 +12,10 @@ sap.ui.define([
 		},
 		init: function() {
 			UIComponent.prototype.init.apply(this, arguments);
+		
+			var oDeviceModel = new JSONModel(Device);
+			oDeviceModel.setDefaultBindingMode("OneWay");
+			this.setModel(oDeviceModel, "Device");
 		
 			this.getRouter().initialize();
 		}
