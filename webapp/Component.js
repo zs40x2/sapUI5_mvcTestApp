223828ecf0f33aee7be044c3a879d813aea7b474
Mehnert Stefan <s.mehnert@polipol.de>
2017-01-11 05:42:13 +0000

    Show up and down arrows only in on phone devices.

--- webapp/Component.js
+++ webapp/Component.js
@@ -15,7 +15,7 @@ sap.ui.define([
 		
 			var oDeviceModel = new JSONModel(Device);
 			oDeviceModel.setDefaultBindingMode("OneWay");
-			this.setModel(oDeviceModel, "Device");
+			this.setModel(oDeviceModel, "device");
 		
 			this.getRouter().initialize();
 		}
