43d9294690c67cc6f857a9d9741e5f7924b61756
Mehnert Stefan <s.mehnert@polipol.de>
2017-01-12 05:26:11 +0000

    Loading and initializing the mock REST service.

--- webapp/Component.js
+++ webapp/Component.js
@@ -1,8 +1,9 @@
 sap.ui.define([
 	"sap/ui/core/UIComponent",
 	"sap/ui/model/json/JSONModel",
-	"sap/ui/Device"
-	], function(UIComponent, JSONModel, Device) {
+	"sap/ui/Device",
+	"sapui5/demo/mvcapp/model/AppModel"
+	], function(UIComponent, JSONModel, Device, AppModel) {
 		"use strict";
 	
 	return UIComponent.extend("sapui5.demo.mvcapp.Component", {
@@ -16,6 +17,23 @@ sap.ui.define([
 			var oDeviceModel = new JSONModel(Device);
 			oDeviceModel.setDefaultBindingMode("OneWay");
 			this.setModel(oDeviceModel, "device");
+			
+			var oAppModel = new AppModel("/Suppliers");
+          
+            jQuery.ajax({
+                type : "GET",
+                contentType : "application/json",
+                url : "/Suppliers",
+                dataType : "json",
+                success : function(oData) {
+                    oAppModel.setData(oData);
+                },
+                error : function() {
+                    jQuery.sap.log.debug("something went wrong while retrieving the data");
+                }
+			});
+			
+            this.setModel(oAppModel);
 		
 			this.getRouter().initialize();
 		}
